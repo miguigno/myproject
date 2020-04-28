3585f34320eb376b91880f73b34999f66c55ea5d
miguigno <63732200+miguigno@users.noreply.github.com>
2020-04-28 11:03:14 +0200

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,22 @@
+
+
+playerage = input("your age")
+
+playername = input("choice your name")
+
+
+text = "The name of the player is {} and he have {} years."
+
+print (text. format(playername, playerage))
+
+
+
+
+
+
+
+
+
+
+
+
